1322e7df3bd03057c79ea3c7c1f88db944cf952a
Juan Carlos Fernández Jara <jfernandezja@uoc.edu>
2020-08-10 18:04:38 +0200

    test

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+local currentContainerId = ${cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -1}
+
 docker build -t oauth-service-func deployment/oauth-service
 docker build -t signer-service-func deployment/signer-service
 
 docker run -d -p 9080:8080 --rm --name oauth-service-func -t oauth-service-func 
 docker run -d -p 9082:8080 --rm --name signer-service-func -t signer-service-func
+
+docker network create signer-network
+docker network connect signer-network $currentContainerId
+docker network connect signer-network oauth-service-func
+docker network connect signer-network signer-service-func
 
 sleep 5
