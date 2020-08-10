5a0d706aa303d5299402f3cb9a88844f4238b29a
Juan Carlos Fernández Jara <jfernandezja@uoc.edu>
2020-08-10 16:56:27 +0200

    deploy

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t oauh-service-func src/test/resources/oauth-service
-docker build -t signer-service-func src/test/resources/signer-service
+docker build -t oauh-service-func deployment/oauth-service
+docker build -t signer-service-func deployment/signer-service
 
 docker run -p 9080:8080 --name oauth-service-func -t oauth-service-func 
 docker run -p 9082:8080 --name signer-service-func -t signer-service-func
