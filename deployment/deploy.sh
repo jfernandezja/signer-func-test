2371765a0c4f9f8f02257f9aa8f121b8d2e94326
Juan Carlos Fernández Jara <jfernandezja@uoc.edu>
2020-08-10 17:50:31 +0200

    sleep

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,3 +5,5 @@ docker build -t signer-service-func deployment/signer-service
 
 docker run -d -p 9080:8080 --rm --name oauth-service-func -t oauth-service-func 
 docker run -d -p 9082:8080 --rm --name signer-service-func -t signer-service-func
+
+sleep 5
