b8b3cdac464132cd74b236143b75c145c9a811a6
Juan Carlos Fernández Jara <jfernandezja@uoc.edu>
2020-08-10 18:37:12 +0200

    test

--- deployment/undeploy.sh
+++ deployment/undeploy.sh
@@ -2,8 +2,8 @@
 
 currentContainerId=$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -1)
 
-docker stop oauth-service-func
-docker stop signer-service-func
+#docker stop oauth-service-func
+#docker stop signer-service-func
 
-docker network disconnect signer-network $currentContainerId
-docker network rm signer-network
+#docker network disconnect signer-network $currentContainerId
+#docker network rm signer-network
