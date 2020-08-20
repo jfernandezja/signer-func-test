39d491537223c9a915edd3a52ef0c2b5e30f2c4f
Juan Carlos Fernández Jara <jfernandezja@uoc.edu>
2020-08-20 14:48:10 +0200

    branching

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,7 +2,7 @@
 
 currentContainerId=$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -1)
 
-docker build -t oauth-service-func deployment/oauth-service
+docker build -t oauth-service-func --build-arg FROM_BRANCH=${GIT_BRANCH#"origin/"} deployment/oauth-service
 docker build -t signer-service-func deployment/signer-service
 
 docker run -d --rm --name oauth-service-func -t oauth-service-func 
