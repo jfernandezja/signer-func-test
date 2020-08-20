a053568e371aac4412bf7534ec56a17999821a2a
Juan Carlos Fernández Jara <jfernandezja@uoc.edu>
2020-08-20 14:55:01 +0200

    branching

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -9,7 +9,7 @@ docker build -t signer-service-func:$BRANCH --build-arg FROM_BRANCH=$BRANCH depl
 docker run -d --rm --name oauth-service-func-$BRANCH -t oauth-service-func:$BRANCH 
 docker run -d --rm --name signer-service-func-$BRANCH -t signer-service-func:$BRANCH
 
-docker network create signer-network
-docker network connect signer-network $currentContainerId
-docker network connect signer-network oauth-service-func-$BRANCH
-docker network connect signer-network signer-service-func-$BRANCH
+docker network create signer-network-$BRANCH
+docker network connect signer-network-$BRANCH $currentContainerId
+docker network connect signer-network-$BRANCH oauth-service-func-$BRANCH
+docker network connect signer-network-$BRANCH signer-service-func-$BRANCH
--- deployment/undeploy.sh
+++ deployment/undeploy.sh
@@ -6,5 +6,5 @@ BRANCH=${GIT_BRANCH#"origin/"}
 docker stop oauth-service-func-$BRANCH
 docker stop signer-service-func-$BRANCH
 
-docker network disconnect signer-network $currentContainerId
-docker network rm signer-network
+docker network disconnect signer-network-$BRANCH $currentContainerId
+docker network rm signer-network-$BRANCH
